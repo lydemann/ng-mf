1cef1c0f72c560d59233f3a3eb1cbad33a5b5429
Christian Lüdemann <christian@christianlydemann.com>
2025-08-06 23:48:21 +0200

    chore: working on mf with rspack

--- apps/dashboard/webpack.config.ts
+++ apps/dashboard/webpack.config.ts
@@ -1,9 +1,24 @@
-import { withModuleFederation } from '@nx/module-federation/angular';
+import {
+    NxModuleFederationPlugin,
+    NxModuleFederationDevServerPlugin,
+} from '@nx/module-federation/angular';
 import config from './module-federation.config';
-
-/**
- * DTS Plugin is disabled in Nx Workspaces as Nx already provides Typing support for Module Federation
- * The DTS Plugin can be enabled by setting dts: true
- * Learn more about the DTS Plugin here: https://module-federation.io/configure/dts.html
- */
-export default withModuleFederation(config, { dts: false });
+  
+  /**
+   * DTS Plugin is disabled in Nx Workspaces as Nx already provides Typing support for Module Federation
+   * The DTS Plugin can be enabled by setting dts: true
+   * Learn more about the DTS Plugin here: https://module-federation.io/configure/dts.html
+   */
+  
+  export default {
+    plugins: [
+      new NxModuleFederationPlugin(
+        { config },
+        {
+          dts: false,
+        }
+      ),
+      new NxModuleFederationDevServerPlugin({ config }),
+    ],
+  };
+  
